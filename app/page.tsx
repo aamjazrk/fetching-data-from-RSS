5a2d7d3ff7393afa0772f79f509d8f1ee40b3ee9
aamjazrk <aamaloha2244@gmail.com>
2023-08-25 08:16:34 +0700

    :hammer: Auto play Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -7,72 +7,52 @@ import CategoryChip from "@/components/ui/module/catagoryChip";
 import Link from "next/link";
 import Swipe from "react-easy-swipe";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import React from "react";
 
 export default function Home() {
   const [items, setItem] = useState<RSSItemInterface[]>([]);
-  const [currentSlide, setCurrentSlide] = useState(1); // Start with slide 1
+  const [currentSlide, setCurrentSlide] = useState(0); // Start with slide 1
+  const [show, setShow] = useState<RSSItemInterface[]>([]);
 
   const handlePrevSlide = () => {
-
-    if(currentSlide===0){
-      setCurrentSlide(items.length - 1)
-    }else{
-      setCurrentSlide(currentSlide - 1)
-    }    console.log("call handlePrevSlide id: ", currentSlide);
-    // setCurrentSlide((prevSlide) =>
-    // currentSlide === 0 ? items.length - 1 : currentSlide - 1
-    // ); // Ensure minimum is 1
+    if (currentSlide === 0) {
+      setCurrentSlide(show.length - 1);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+    console.log("call handlePrevSlide id: ", currentSlide);
   };
 
   const handleNextSlide = () => {
     console.log("call handleNextSlide id: ", currentSlide);
     setCurrentSlide((prevSlide) =>
-      prevSlide > items.length - 2 ? 0 : prevSlide + 1
+      prevSlide > show.length - 2 ? 0 : prevSlide + 1
     ); // Ensure maximum is the number of items
   };
-  // const handlePrevSlide = () => {
-  //   setCurrentSlide((prevSlide) =>
-  //     prevSlide === 1 ? items.length : prevSlide - 1
-  //   );
 
-  // };
-
-  // const handleNextSlide = () => {
-  //   console.log(currentSlide);
-  //   setCurrentSlide((prevSlide) =>
-  //   prevSlide >= items.length - 1 ? 1 : prevSlide + 1
-  //   );
-    
-  // };
-
-  const handleIndexClick = (index: number) => {
-    setCurrentSlide(index);
-  };
-
-  // const [data, setData] = useState<JSON[]>([]);
   const getDataRss = async () => {
     let res = await getData();
     console.log("resdata", res);
     if (res.data.items) {
-      // setData(res.data.items);
       setItem(res.data.items);
+      setShow(res.data.items.slice(0, 4));
+      setCurrentSlide(1);
     }
   };
+  // Autoplay configuration
+  const autoplayDelay = 5000; // Delay in milliseconds
+  useEffect(() => {
+    const autoplayInterval = setInterval(() => {
+      handleNextSlide();
+    }, autoplayDelay);
 
-
+    return () => clearInterval(autoplayInterval);
+  }, [currentSlide]);
 
   useEffect(() => {
     getDataRss();
-    // Auto slide every 5 seconds
-    // const interval = setInterval(handleNextSlide, 5000);
-    // console.log('let see',currentSlide);
-    // Clean up the interval when the component unmounts
-    // return () => clearInterval(interval);
-    // Auto slide every 5 seconds
-    // const timer = setTimeout(() => handleNextSlide, 1000);
-    // return () => clearTimeout(timer);
-
   }, []);
+
   // Log the item state whenever it changes
   useEffect(() => {
     console.log("Updated item:", items);
@@ -82,7 +62,7 @@ export default function Home() {
       <h1 className="text-2xl font-bold mb-4 text-black">Blogs</h1>
       <div className="relative mx-auto  container">
         {/* Carousel */}
-        <div className="w-2/3 h-[50vh] flex overflow-hidden relative m-auto">
+        <div className="w-2/4 h-[50vh] flex overflow-hidden relative m-auto">
           {/* Left button */}
           <AiOutlineLeft
             onClick={handlePrevSlide}
@@ -93,17 +73,19 @@ export default function Home() {
             onSwipeRight={handleNextSlide}
             className="relative z-10 w-full h-full"
           >
-            {items.map((item, index) => (
-              <Image
-                key={item.id}
-                src={item.image}
-                layout="fill"
-                objectFit="cover"
-                className={`animate-fadeIn ${
-                  index === currentSlide ? "visible" : "hidden"
-                }`}
-                alt="Picture of slice"
-              />
+            {show.map((item, index) => (
+              <Link href={item.link}>
+                <Image
+                  key={item.id}
+                  src={item.image}
+                  layout="fill"
+                  objectFit="cover"
+                  className={`animate-fadeIn ${
+                    index === currentSlide ? "visible" : "hidden"
+                  }`}
+                  alt="Picture of slice"
+                />
+              </Link>
             ))}
           </Swipe>
           {/* Right button */}
@@ -114,7 +96,7 @@ export default function Home() {
         </div>
         {/* Pagination */}
         <div className="relative flex justify-center p-2">
-          {items.map((item) => (
+          {show.map((item) => (
             <div
               key={item.id}
               className={
@@ -130,7 +112,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="grid md:grid-cols-3 gap-5">
+      <div className="w-10/12 grid md:grid-cols-3 gap-5">
         {items &&
           items.map((item) => (
             <div
@@ -139,34 +121,28 @@ export default function Home() {
               className="bg-white shadow-md rounded-md p-4 max-w-5xl max-h-full "
             >
               <Link href={item.link}>
-              <h2 className="mb-2 text-gray-600 text-lg font-semibold">
-                
+                <h2 className="mb-2 text-gray-600 text-lg font-semibold">
                   <span className="line-clamp-2">{item.title}</span>
-                
-              </h2>
-              <span className="flex flex-wrap flex-1 ">
-                {item.categories.map((item) => (
-                  <CategoryChip category={item}></CategoryChip>
-                ))}
-              </span>
-              <div className="flex justify-center">
-              <Image
-                src={item.image}
-                width={300}
-                height={300}
-                alt="Picture of the author"
-                className="place-self-center pt-2"
-                objectFit="cover"
-              />
-              </div>
-              
-              <p className="text-gray-600">creator: {item.creator}</p>
-              <p className="text-gray-600">date: {item.isoDate}</p>
-              <p className="text-gray-600 line-clamp-2">
-                {item["content:encodedSnippet"]}
-              </p>
+                </h2>
+                <span className="flex flex-wrap flex-1 ">
+                  {item.categories.map((item) => (
+                    <CategoryChip category={item}></CategoryChip>
+                  ))}
+                </span>
+                <div className="flex justify-center">
+                  <Image
+                    src={item.image}
+                    width={300}
+                    height={300}
+                    alt="Picture of the author"
+                    className="place-self-center pt-2"
+                    objectFit="cover"
+                  />
+                </div>
+                <p className="text-gray-600 line-clamp-3 pt-3">
+                  {item["content:encodedSnippet"]}
+                </p>
               </Link>
-
             </div>
           ))}
       </div>
