c43096ce5dde96adba501f3fd56e49ec9f8f907a
aamjazrk <aamaloha2244@gmail.com>
2023-08-25 15:33:33 +0700

    :hammer: Dev container

--- components/ui/module/catagoryChip.tsx
+++ components/ui/module/catagoryChip.tsx
@@ -1,7 +1,7 @@
 
 
 interface CategoryChipProps {
-    category: string; // Define the type of the 'category' prop
+    category: string; 
   }
 const CategoryChip: React.FC<CategoryChipProps> = ({category}) => (
 <div className="bg-cyan-100 text-gray-500 text-xs font-medium m-0.5 px-2.5 py-0.5 rounded ">
